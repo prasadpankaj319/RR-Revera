a76b6b5d0eda90bae02ae8b865e83cd7270d5416
Pankaj Prasad <prasadpankaj319@gmail.com>
2026-04-05 13:19:41 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('MongoDB Connected successfully!'))
     .catch(err => console.error('MongoDB error:', err));
 
